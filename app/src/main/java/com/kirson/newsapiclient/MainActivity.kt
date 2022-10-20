e6ce837b24fcc4efadcb4617adcc3310612f4d00
kirson <kirill0603@gmail.com>
2022-10-20 20:38:19 +0300

    Add recyclerView to show news on NewsFragment

--- app/src/main/java/com/kirson/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/kirson/newsapiclient/MainActivity.kt
@@ -2,12 +2,23 @@ package com.kirson.newsapiclient
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.kirson.newsapiclient.databinding.ActivityMainBinding
+import com.kirson.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.kirson.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+
     private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +30,11 @@ class MainActivity : AppCompatActivity() {
                 .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnvNews.setupWithNavController(navController)
+        viewModel = ViewModelProvider(
+            this,
+            factory
+        )[NewsViewModel::class.java]
+
 
     }
 }
--- app/src/main/java/com/kirson/newsapiclient/NewsFragment.kt
+++ app/src/main/java/com/kirson/newsapiclient/NewsFragment.kt
@@ -1,21 +1,26 @@
 package com.kirson.newsapiclient
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kirson.newsapiclient.data.util.Resource
+import com.kirson.newsapiclient.databinding.FragmentNewsBinding
+import com.kirson.newsapiclient.presentation.adapter.NewsAdapter
+import com.kirson.newsapiclient.presentation.viewmodel.NewsViewModel
 
 
 class NewsFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var viewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var fragmentNewsBinding: FragmentNewsBinding
+    private var country = "ru"
+    private var page = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +28,71 @@ class NewsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentNewsBinding = FragmentNewsBinding.bind(view)
+        viewModel = (activity as MainActivity).viewModel
+
+        initRecyclerView()
+        viewNewsList()
+
+    }
+
+
+    private fun viewNewsList() {
+
+        viewModel.getNewsHeadLines(country, page)
+        viewModel.newsHeadLines.observe(
+            viewLifecycleOwner, Observer { response ->
+
+                when (response) {
+                    is Resource.Success -> {
+                        hideProgressBar()
+                        response.data?.let {
+                            Log.i("MYTAG", it.articles.toString())
+                            newsAdapter.differ.submitList(it.articles.toList())
+                        }
+
+                    }
+                    is Resource.Loading -> {
+                        showProgressBar()
+
+                    }
+                    is Resource.Error -> {
+                        hideProgressBar()
+                        response.message?.let {
+                            Toast.makeText(activity, "An error occured: $it", Toast.LENGTH_LONG)
+                                .show()
+                        }
+
+                    }
+                }
+
+
+            })
+
+
+    }
+
+    private fun initRecyclerView() {
+        newsAdapter = NewsAdapter()
+        fragmentNewsBinding.rvNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+
+
+    }
+
+    private fun showProgressBar() {
+        fragmentNewsBinding.progressBar.visibility = View.VISIBLE
+    }
+
+
+    private fun hideProgressBar() {
+        fragmentNewsBinding.progressBar.visibility = View.INVISIBLE
     }
 
 
