30b847180bcbebade396966ddbfa07ad4a669e47
kirson <kirill0603@gmail.com>
2022-10-20 19:26:04 +0300

    Add navigation (SavedFragment, NewsFragment, InfoFragment) Connect bottom navigation with fragments

--- app/src/main/java/com/kirson/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/kirson/newsapiclient/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.kirson.newsapiclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.kirson.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager
+                .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
+
     }
 }
---
+++ app/src/main/java/com/kirson/newsapiclient/NewsFragment.kt
@@ -0,0 +1,29 @@
+package com.kirson.newsapiclient
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+class NewsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+
+}
