be46b1dedfa1cf55011394d1b92264ce22497640
kirson <kirill0603@gmail.com>
2022-10-20 16:54:27 +0300

    Testing API service functions using MockWebServer

---
+++ app/src/test/java/com/kirson/newsapiclient/data/api/NewsAPIServiceTest.kt
@@ -0,0 +1,80 @@
+package com.kirson.newsapiclient.data.api
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NewsAPIServiceTest {
+    private lateinit var service: NewsAPIService
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsAPIService::class.java)
+    }
+
+    private fun enqueueMockResponse(
+        fileName: String
+    ) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+    }
+
+    @Test
+    fun getTopHeadlines_sentRequest_receivedExpected() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("ru", 1).body()
+            val request = server.takeRequest()
+            assertThat(responseBody).isNotNull()
+            assertThat(request.path).isEqualTo("/v2/top-headlines?country=ru&page=1&apiKey=62049572abc048f68d102fae670f843b")
+        }
+    }
+
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctPageSize() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("ru", 1).body()
+            val articlesList = responseBody!!.articles
+            assertThat(articlesList.size).isEqualTo(20)
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctContent() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("ru", 1).body()
+            val articlesList = responseBody!!.articles
+            val article = articlesList[0]
+            assertThat(article.author).isEqualTo("Авто Mail.ru")
+            assertThat(article.url).isEqualTo("https://auto.mail.ru/article/85562-na-ryinok-rf-vyihodyat-sereznyie-vnedorozhniki-iz/")
+            assertThat(article.publishedAt).isEqualTo("2022-10-20T09:56:15Z")
+        }
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+}
